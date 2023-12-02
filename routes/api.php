c19b3dde90dd27c034ca31e183e5a778981fc627
jiahu <jiahu@abunistyle.com>
2023-12-02 21:29:50 +0800

    升级到10.x

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,31 +9,11 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::post('/wechat/pushMsgJson/', 'WechatApiController@pushMsgJson');
-Route::post('/wechat/pushMsgExt/', 'WechatApiController@pushMsgExt');
-Route::get('/wechat/getWxHis/', 'WechatApiController@getWxHis');
-Route::get('/wechat/getWxPost/', 'WechatApiController@getWxPost');
-Route::get('/wechat/getMsgList/', 'WechatApiController@getMsgList');
-
-Route::get('/wechat/account/list', 'WechatAccountController@getAccountList');
-Route::put('/wechat/account/{id}', 'WechatAccountController@updateAccount');
-Route::delete('/wechat/account/{id}', 'WechatAccountController@deleteAccount');
-Route::post('/wechat/account', 'WechatAccountController@insertAccount');
-Route::get('/wechat/statistics', 'WechatStatisticsController@getStatistics');
-Route::get('/wechat/msg', 'WechatMsgController@getList');
-
-Route::get('/weibo/account/list', 'WeiboAccountController@getAccountList');
-Route::put('/weibo/account/{id}', 'WeiboAccountController@updateAccount');
-Route::delete('/weibo/account/{id}', 'WeiboAccountController@deleteAccount');
-Route::post('/weibo/account', 'WeiboAccountController@insertAccount');
-Route::get('/weibo/statistics', 'WeiboStatisticsController@getStatistics');
-Route::get('/weibo/msg', 'WeiboMsgController@getList');
